fa7723c4928b897935ed4d045df7414356fde2b7
Faisal Alagla <alagla.faisal@gmail.com>
2025-01-21 14:28:40 +0300

    entity classes

---
+++ src/main/java/com/faisal/usermanager/group/Group.java
@@ -0,0 +1,33 @@
+package com.faisal.usermanager.group;
+
+import com.faisal.usermanager.utils.baseclasses.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class Group extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "group_image_url")
+    private String groupImageUrl;
+
+}
---
+++ src/main/java/com/faisal/usermanager/user/User.java
@@ -0,0 +1,45 @@
+package com.faisal.usermanager.user;
+
+import com.faisal.usermanager.utils.baseclasses.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "role_id")
+    private UUID roleId;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "group_id")
+    private UUID groupId;
+
+}
