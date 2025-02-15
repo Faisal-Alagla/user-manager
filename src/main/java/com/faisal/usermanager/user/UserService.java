b0dc6f4caf4a3ae2a600c3865c4ec2331d50575d
Faisal Alagla <alagla.faisal@gmail.com>
2025-02-15 20:24:06 +0300

    upload user profile image upon user creation / update

--- src/main/java/com/faisal/usermanager/user/UserService.java
+++ src/main/java/com/faisal/usermanager/user/UserService.java
@@ -1,6 +1,7 @@
 package com.faisal.usermanager.user;
 
 import com.faisal.usermanager.common.exceptions.ErrorMessage;
+import com.faisal.usermanager.common.exceptions.ObjectStoreException;
 import com.faisal.usermanager.common.exceptions.ResourceException;
 import com.faisal.usermanager.integration.objectstore.IObjectStoreService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -22,7 +25,19 @@ public class UserService implements IUserService {
     @Override
     @Transactional
     public UserResponseDto createUser(UserCreationDto userCreationDto) {
-        User createdUser = userRepository.save(UserMapper.mapToUser(userCreationDto));
+
+        String profileImagePath = null;
+        if (userCreationDto.getProfileImage() != null) {
+            try {
+                profileImagePath = uploadUserPhoto(userCreationDto.getProfileImage());
+            } catch (Exception e) {
+                throw new ObjectStoreException(ErrorMessage.OBJECT_STORE_UPLOAD_FAILED);
+            }
+        }
+
+        User newUser = UserMapper.mapToUser(userCreationDto);
+        newUser.setProfileImageUrl(profileImagePath);
+        User createdUser = userRepository.save(newUser);
 
         return UserMapper.mapToUserResponseDto(createdUser);
     }
@@ -48,7 +63,16 @@ public class UserService implements IUserService {
         User user = userRepository.findByIdAndIsActiveTrue(userId)
                 .orElseThrow(() -> new ResourceException(ErrorMessage.USER_NOT_FOUND));
 
-        updateUserData(user, userUpdateDto);
+        String newProfileImagePath = null;
+        if (userUpdateDto.getProfileImage() != null) {
+            try {
+                newProfileImagePath = uploadUserPhoto(userUpdateDto.getProfileImage());
+            } catch (Exception e) {
+                throw new ObjectStoreException(ErrorMessage.OBJECT_STORE_UPLOAD_FAILED);
+            }
+        }
+
+        updateUserData(user, userUpdateDto, newProfileImagePath);
         User updatedUser = userRepository.save(user);
 
         return UserMapper.mapToUserResponseDto(updatedUser);
@@ -70,11 +94,27 @@ public class UserService implements IUserService {
         return userRepository.findByIdAndIsActiveTrue(userId).isPresent();
     }
 
-    private void updateUserData(User user, UserUpdateDto userUpdateDto) {
+    private void updateUserData(User user, UserUpdateDto userUpdateDto, String newProfileImagePath) {
         user.setEmail(userUpdateDto.getEmail()); //TODO: make sure to update in keycloak later
         user.setPhone(userUpdateDto.getPhone());
         user.setRoleId(userUpdateDto.getRoleId()); //TODO: check role comparison (higher can change lower)
-        //TODO: update image url
+
+        if (newProfileImagePath != null && !newProfileImagePath.isEmpty()) {
+            user.setProfileImageUrl(newProfileImagePath);
+        }
+    }
+
+    // Returns the path of the uploaded file
+    private String uploadUserPhoto(MultipartFile file) throws IOException {
+        String filePath = "user-profile-images/" + UUID.randomUUID();
+
+        objectStoreService.uploadObject(
+                file.getBytes(),
+                filePath,
+                file.getContentType()
+        );
+
+        return filePath;
     }
 
 }
